0035546037082b13c2c2620cb5ce502abea2b54c
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-11 21:19:03 +0430

    Create footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+
+function Footer() {
+  return (
+    <Grid container justifyContent="center">
+      <Typography color="azure" sx={{ padding: "2rem 0" }}>
+        Created with ❤️ by HosseinDeveloper
+      </Typography>
+    </Grid>
+  );
+}
+
+export default Footer;
