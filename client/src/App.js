53c8a724273af4bac1020d0405acafa8e23af2a1
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-11 21:16:33 +0430

    Create header with MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
+import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+import Footer from "./components/Footer";
+import "./App.css";
+
 function App() {
-  return <div>hiii</div>;
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,175 @@
+import { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem,
+  Container,
+  Avatar,
+  Button,
+  Tooltip,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from "@mui/icons-material/Home";
+import LoginIcon from "@mui/icons-material/Login";
+import MenuBar from "./MenuBar";
+
+const pages = ["Home", "Music", "About Us"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const Header = () => {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+  const [isLogin, setIsLogin] = useState(false);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = (setting) => {
+    setting === "Logout" && setIsLogin(!isLogin);
+    setAnchorElUser(null);
+  };
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#1976d2",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar position="sticky">
+        <Container maxWidth="lg">
+          <Toolbar disableGutters>
+            <Box
+              component="section"
+              sx={{
+                mr: 5,
+                display: { xs: "none", md: "flex" },
+              }}
+            >
+              <img
+                className="header-logo"
+                src={require("../image/logo.png")}
+                alt=""
+              />
+            </Box>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <MenuBar
+                items={pages}
+                anchorEl={anchorElNav}
+                handleClose={handleCloseNavMenu}
+              />
+            </Box>
+            <Box
+              component="section"
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+              }}
+            >
+              <img
+                className="header-logo"
+                src={require("../image/logo.png")}
+                alt=""
+              />
+            </Box>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "flex" }}
+                  startIcon={page === "Home" && <HomeIcon />}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            {isLogin ? (
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="Profile"
+                      src={require("../image/profile.jpg")}
+                    />
+                  </IconButton>
+                </Tooltip>
+                {/* <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem
+                      key={setting}
+                      onClick={() => handleCloseUserMenu(setting)}
+                    >
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu> */}
+                <MenuBar
+                  items={settings}
+                  anchorEl={anchorElUser}
+                  handleClose={handleCloseUserMenu}
+                />
+              </Box>
+            ) : (
+              <Button
+                onClick={() => setIsLogin(!isLogin)}
+                sx={{ my: 2, color: "white", display: "flex" }}
+                startIcon={<LoginIcon />}
+              >
+                Login
+              </Button>
+            )}
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+export default Header;
