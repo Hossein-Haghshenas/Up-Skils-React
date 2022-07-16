32eca538bea0fdcbeb239111bcc0c558bdea2873
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-16 19:25:37 +0430

    Create some tests for app component

--- client/src/tests/app.test.js
+++ client/src/tests/app.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import App from "./../App";
 
-test("renders learn react link", () => {
+test("render header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const footer = screen.getByTestId("header");
+  expect(footer).toBeInTheDocument();
+});
+
+test("render homepage", () => {
+  render(<App />);
+  const footer = screen.getByTestId("homepage");
+  expect(footer).toBeInTheDocument();
+});
+
+test("render footer", () => {
+  render(<App />);
+  const footer = screen.getByTestId("footer");
+  expect(footer).toBeInTheDocument();
 });
