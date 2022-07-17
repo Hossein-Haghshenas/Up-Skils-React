839e4a34e2427f26f1a6371ada90ff13c1e9e0bb
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-17 10:14:53 +0430

    Change something

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,21 +3,18 @@ import Article from "../components/Article";
 import decor1 from "../assets/image/decor-1.png";
 import decor2 from "../assets/image/decor-2.gif";
 import decor3 from "../assets/image/decor-3.png";
-
-const firstArticleText = "Experience The Best Qulaity Music";
-const secondArticleText = "Search Music by Name or Direct URL";
-const thirdArticleText = "Enjoy listening to them";
+import articleTexts from "../constants/HomePageArticleTexts.json";
 
 function HomePage() {
   return (
     <section data-testid="homepage" className="home-page">
-      <Article direction={"row"} text={firstArticleText} image={decor1} />
+      <Article direction={"row"} text={articleTexts.firstText} image={decor1} />
       <Article
         direction={"row-reverse"}
-        text={secondArticleText}
+        text={articleTexts.secondText}
         image={decor2}
       />
-      <Article direction={"row"} image={decor3} text={thirdArticleText} />
+      <Article direction={"row"} image={decor3} text={articleTexts.thirdText} />
     </section>
   );
 }
