46eb58ea8736c3c274f62809bb477debd0152a28
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-20 19:02:35 +0430

    Create some test for buttons

--- client/src/tests/ButtonCo.test.js
+++ client/src/tests/ButtonCo.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import ButtonCo from "./../components/ButtonCo";
 
 it("render button", () => {
   render(<ButtonCo children="Home" />);
   const button = screen.getByRole("button", { name: "Home" });
   expect(button).toBeInTheDocument();
+  expect(button).toBeEnabled();
+});
+
+it("should button be disabled", () => {
+  render(<ButtonCo children="Home" disabled />);
+  const button = screen.getByText(/Home/i);
+  expect(button).toHaveAttribute("disabled");
+  expect(button).toBeDisabled();
+});
+
+it("should have event", () => {
+  const mockHandleClick = jest.fn();
+  render(<ButtonCo children="Home" onClick={mockHandleClick} />);
+  const button = screen.getByText(/Home/i);
+  // run event
+  fireEvent.click(button);
+  // check the event should call one time
+  expect(mockHandleClick).toBeCalledTimes(1);
 });
