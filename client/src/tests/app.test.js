4831e33760100b30029847dbbb1828dc5ff690f6
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-21 23:48:56 +0430

    Create a snapshot test for app component

--- client/src/tests/app.test.js
+++ client/src/tests/app.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
+import { renderer } from "react-test-renderer";
 import App from "./../App";
+import { ReactDOM } from "react-dom/client";
 
 test("render header", () => {
   render(<App />);
@@ -17,4 +19,24 @@ test("render footer", () => {
   render(<App />);
   const footer = screen.getByRole("contentinfo");
   expect(footer).toBeInTheDocument();
+});
+
+// snapshot test
+
+describe("Popover", () => {
+  beforeAll(() => {
+    ReactDOM.createPortal = jest.fn((element, node) => {
+      return element;
+    });
+  });
+
+  afterEach(() => {
+    ReactDOM.createPortal.mockClear();
+  });
+
+  it("should render correctly with Node or Function", () => {
+    const component = renderer.create(<App />);
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
 });
